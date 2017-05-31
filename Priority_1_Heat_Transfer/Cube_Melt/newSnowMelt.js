63f80f6dc3962693a618cfb5fd612628b920b25e
bbullek <bbullek@live.com>
2017-05-31 14:01:41 -0400

    Refactored snowMelt code and improved OO design

--- Priority_1_Heat_Transfer/Cube_Melt/newSnowMelt.js
+++ Priority_1_Heat_Transfer/Cube_Melt/newSnowMelt.js
@@ -1,30 +1,43 @@
-/*File:snowMelt.html
-	Author: Daniel Vasquez (May 2017)
-			Under the supervision of Margot Vigeant, Bucknell University
-*/
+/* File: snowMelt.html
+ * Authors: Daniel Vasquez and Brooke Bullek (May 2017)
+ * Under the supervision of Margot Vigeant, Bucknell University
+ */
+
+/******************* Constants **********************/
+
+var ICE_FREEZE_TEMP_K = 273.15; // Temperature of ice at freezing point in Kelvin
+var ICE_DENSITY = 917; // Density of ice in kg/m^3
+var MAX_DIVISIONS = 5; // Maximum number of times user can break the ice block
+var BASE_WIDTH = 100; // Number of pixels along one edge of an unbroken ice block
+
+/**************** Global variables ******************/
+
 var iceCanvas;
 var numDivisions = -1;
-var maxDivisions = 5;
 var array = [];
 var arrayPos = {x:100, y:100};
-var baseWidth = 100;
 var holdingHammer = false;
 var ctx;
 var myLineChart;
-var chartData = { //Configuration data for chart
+
+/********** Configuration data for chart ************/
+
+var chartData = {
   type: 'line',
   data: {
-    datasets: [{
+    datasets: [
+      {
         label: 'Broken Ice',
-        data: [{x:0,y:1}, {x:1,y:2}]
+        data: [{x:0, y:1}, {x:1, y:2}]
       },
       {
         label: 'Unbroken Ice',
-        backgroundColor: "rgba(75,192,192,0.4)",
-        data: [{x:0,y:2}, {x:1,y:0}]
+        backgroundColor: "rgba(75, 192, 192, 0.4)",
+        data: [{x:0, y:2}, {x:1, y:0}]
       }
     ]
   },
+
   options: {
     pan: {
       enabled: true,
@@ -42,309 +55,300 @@ var chartData = { //Configuration data for chart
   }
 };
 
-/* -- Science Variables -- */
-var Tice = 273.15 // Temperature of ice at freezing point in Kelvin
-var iceDensity = 917; // Density of ice in kg/m^3
-var brknExp = {
-  array: [],
-  arrayPos: {x:0, y:0},
-  canvas: null,
-  iceDensity: iceDensity,
-  Twater: 280,
-  edgeLength: baseWidth, // The length of an individual piece's edge
-  surfaceArea: self.edgeLength * self.edgeLength * 6,
-  volume: self.edgeLength * self.edgeLength * self.edgeLength,
-  Mice: self.iceDensity*self.volume,
-  numDivisions: 0
+/***** Reusable class for cube melt experiment ******/
+
+function CubeMeltExp() {
+  // Class attributes
+  this.array = []; // Array of rectangles (ice pieces)
+  this.arrayPos = {x:0, y:0};
+  this.canvas = null;
+  this.waterTemp = 280; // Temperature of water in Kelvin // TODO: why 280?
+  this.edgeLength = BASE_WIDTH; // The length of an ice piece's edge
+  this.surfaceArea = this.edgeLength * this.edgeLength * 6;
+  this.volume = Math.pow(this.edgeLength, 3);
+  this.iceMass = ICE_DENSITY * this.volume;
+
+  /* Unbroken exp always has 0 divisions. This will vary for the broken exp. */
+  this.numDivisions = 0;
+
+  /* 
+   * Draws this experiment's array of cube(s).
+   */
+  this.display = function() {
+    var length = Math.pow(2, this.numDivisions);
+    for (var i = 0; i < length; i++) {
+      for (var j = 0; j < length; j++) {
+        var piece = this.array[i][j];
+        rect(piece.x + this.arrayPos.x, piece.y + this.arrayPos.y, piece.width, piece.height);
+        //print("Made a rect at:", piece.x, piece.y); // Debug
+      }
+    }
+  }
+
+  /*
+   * Initializes the array of ice blocks in this experiment.
+   */
+  this.initializeArray = function() {
+    var length = Math.pow(2, MAX_DIVISIONS);
+    for (var i = 0; i < length; i++) {
+      var list = [];
+      for (var j = 0; j < length; j++) {
+        list.push({x:0, y:0, width:0, height:0});
+      }
+
+      this.array.push(list);
+    }
+  }
+
+  /*
+   * Initializes this experiment's canvas.
+   * @param targetElement: The ID of the HTML div element that will hold this canvas.
+   */
+  this.initializeIceCanvas = function(targetElement) {
+    this.canvas = createCanvas(windowWidth, windowHeight / 2);
+    this.canvas.parent(targetElement);
+  }
+
+  /* 
+   * Divides this experiment's ice into pieces of equal size.
+   * @param n: The number of divisions to be executed.
+   */
+  this.setDivisions = function(n) {
+    if (n < this.numDivisions) {
+      this.initializeArray(); // Reset ice to whole block
+    }
+
+    this.numDivisions = n;
+    var length = Math.pow(2, this.numDivisions); // The number of pieces along one axis
+    var pieceWidth = BASE_WIDTH / length;
+    var paddingToPieceRatio = 0.5;
+    for (var i = 0; i < length; i++) { // Iterate over pieces that exist
+      for (var j = 0; j < length; j++) {
+        var offset = ((1 + paddingToPieceRatio) * BASE_WIDTH / length);
+        this.array[i][j].x = i * offset;
+        this.array[i][j].y = j * offset;
+        this.array[i][j].width = pieceWidth;
+        this.array[i][j].height = pieceWidth;
+      }
+    }
+  }
+
+  /* 
+   * Returns the length of either side of the split-up ice pieces. Assumes each 
+   * piece's length and width are identical.
+   */
+  this.findArrayRange = function() {
+    var length = Math.pow(2, this.numDivisions); // The number of pieces along one axis
+    var pieceWidth = BASE_WIDTH / length;
+    //print(exp.array[length-1][-0.5]); // Debug
+    var xRange = this.array[length - 1][length - 1].x + pieceWidth;
+    return xRange;
+  }
+
+  /* 
+   * Sets the array's position relative to its center.
+   * @param x: The horizontal placement of the array on the screen
+   * @param y: The vertical placement of the array on the screen
+   */
+  this.setCenterArrayPos = function(x, y) {
+    var offset = this.findArrayRange() / 2;
+    this.arrayPos.x = x - offset;
+    this.arrayPos.y = y - offset;
+    //print(arrayPos.x, arrayPos.y); // Debug
+  }
+
+  /* 
+   * Centers the array in the window.
+   */
+  this.moveArrayToCenter = function() {
+    var middleX = windowWidth / 2;
+    var middleY = windowHeight / 4;
+    this.setCenterArrayPos(middleX, middleY);
+  }
 }
-var unbrknExp = {
-  array: [],
-  arrayPos: {x:0, y:0},
-  canvas: null,
-  iceDensity: iceDensity,
-  Twater: 280,
-  edgeLength: baseWidth,
-  surfaceArea: self.edgeLength * self.edgeLength * 6,
-  volume: self.edgeLength * self.edgeLength * self.edgeLength,
-  Mice: self.iceDensity*self.volume
+
+/***************** Experiment setup *****************/
+
+brokenExp = new CubeMeltExp();
+unbrokenExp = new CubeMeltExp();
+
+function initializeChart() {
+  ctx = document.getElementById("myChart").getContext("2d");
+  myLineChart = new Chart(ctx, chartData);
 }
 
 function setup() {
-  initializeIceCanvas(brknExp, "brokenIceCanvas-holder");
-  initializeIceCanvas(unbrknExp, "unBrokenIceCanvas-holder");
-  initializeArray(maxDivisions, brknExp);
-  initializeArray(maxDivisions, unbrknExp);
-  setDivisions(0, brknExp);
-  setDivisions(0, unbrknExp);
+  brokenExp.initializeIceCanvas("brokenIceCanvas-holder");
+  unbrokenExp.initializeIceCanvas("unBrokenIceCanvas-holder");
+
+  brokenExp.initializeArray();
+  unbrokenExp.initializeArray();
+
+  brokenExp.setDivisions(0);
+  unbrokenExp.setDivisions(0);
+ 
   toggleHammer();
   initializeChart();
 
   //noLoop();
 }
 
-function initializeArray(maxDivisions, exp) {
-  var length = pow(2, maxDivisions);
-  for (var i = 0; i < length; i++){
-    var list = [];
-    for (var j = 0; j < length; j++){
-      list.push({x:0, y:0, width:0, height:0});
-    }
-    exp.array.push(list);
-  }
-}
-
-/*
- * Author: Daniel Vasquez (2017)
- */
-function initializeIceCanvas(exp, targetElement) {
-  exp.canvas = createCanvas(windowWidth, windowHeight/2);
-  exp.canvas.parent(targetElement)
-}
-
-/*
- * Author: Daniel Vasquez (2017)
- */
-function initializeChart() {
-  ctx = document.getElementById("myChart").getContext("2d");
-  myLineChart = new Chart(ctx, chartData);
-}
-
 function draw() {
-  //whipe the canvas clean
-  background(255,255,255);
+  // Clear the canvas
+  background(255, 255, 255);
 
   //myLineChart.data.datasets[0].data[0] += 1;
   //myLineChart.update();
-  moveArrayToCenter(brknExp);
-  moveArrayToCenter(unbrknExp);
+  brokenExp.moveArrayToCenter();
+  unbrokenExp.moveArrayToCenter();
 
-  /*---BEGIN Logic for controlling appearance of cursor---*/
+  /* Begin logic for controlling appearance of cursor */
   if (holdingHammer) {
     if (mouseIsPressed) {
-      cursor('hammer_click.cur', 0, 0); //sets cursor to hammer_click.cur
+      cursor('hammer_click.cur', 0, 0); // Sets cursor to hammer_click.cur
     } else {
       cursor('hammer_hover.cur', 0, 0);
     }
   } else {
-    cursor(ARROW, 0, 0);  //sets cursor to default arrow constant
+    cursor(ARROW, 0, 0); // Sets cursor to default arrow
   }
-  /*---END Logic for controlling appearance of cursor---*/
-  drawExperiment(brknExp);
-  drawExperiment(unbrknExp);
-}
+  /* End logic for controlling appearance of cursor */
 
-function drawExperiment(exp) {
-  var length = pow(2, exp.numDivisions);
-  for (var i = 0; i < length; i++){
-    for (var j = 0; j < length; j++){
-      piece = exp.array[i][j];
-      rect(piece.x + exp.arrayPos.x, piece.y + exp.arrayPos.y, piece.width, piece.height);
-      //print("Made a rect at:", piece.x, piece.y);
-    }
-  }
+  brokenExp.display();
+  unbrokenExp.display();
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight/2); // TODO Remove dependence on window
+  resizeCanvas(windowWidth, windowHeight / 2); // TODO Remove dependence on window
 }
 
-/* rgb(75, 193, 101) - BEGIN Maths and Sciences Functions ------------------*/
-
-/* Divides the experiment's ice into pieces of equal size. Results reflect
- * Uses global values for calculations
- * n divisions from an initial whole ice block.
- * n = number of divisions to be executed
- * exp = the experiment this function will be executed on
- * Author: Daniel Vasquez (2017)
- */
-function setDivisions(n, exp) {
-  if (n < exp.numDivisions) {
-    initializeArray(maxDivisions, exp); // Reset ice to whole block
-  }
-  exp.numDivisions = n;
-  var length = pow(2, exp.numDivisions); // the number of pieces along one axis
-  var pieceWidth = baseWidth/length; // baseWidth is a global
-  var paddingToPieceRatio = .5;
-  for (var i = 0; i < length; i++){ // iterate over pieces that exist
-    for (var j = 0; j < length; j++){
-      // paddingToPieceRatio is a global
-      exp.array[i][j].x = (i)*((1+paddingToPieceRatio)*baseWidth/length);
-      exp.array[i][j].y = (j)*((1+paddingToPieceRatio)*baseWidth/length);
-      exp.array[i][j].width = pieceWidth;
-      exp.array[i][j].height = pieceWidth;
-    }
-  }
-}
-
-/* Returns length of either side of the split-up ice pieces.
- * Assumes each piece's length == width
- * Author: Daniel Vasquez (2017)
- */
-function findArrayRange(exp) {
-  var length = pow(2, exp.numDivisions);
-  var pieceWidth = baseWidth/length;
-  //print(exp.array[length-1][-0.5]);
-  var xRange = exp.array[length-1][length-1].x + pieceWidth;
-  return xRange;
-}
-
-/* Sets the array's position relative to it's center
- * Author: Daniel Vasquez (2017)
- */
-function setCenterArrayPos(exp, x, y) {
-  offset = findArrayRange(exp)/2;
-  exp.arrayPos.x = x - offset;
-  exp.arrayPos.y = y - offset;
-  //print(arrayPos.x, arrayPos.y);
-}
-
-/* Centers the array in the windows
- * Author: Daniel Vasquez (2017)
- */
-function moveArrayToCenter(exp) {
-  var middleX = windowWidth / 2;
-  var middleY = windowHeight / 4;
-  setCenterArrayPos(exp, middleX, middleY);
-}
+/************ Math and science functions ************/
 
 /*
- * Function: findMeltingTime
- * Finds the melting time for the cubes in each beaker to set up the animation fade time
- * Author: Daniel Prudente (September 2012)
+ * Finds the melting time for the cubes in each beaker to set up the animation 
+ * fade time.
  */
 function findMeltingTime() {
-	simulationSetup();
-	findMeltStep();
-	simulationSetup();
-	findMeltStep2();
-	simulationSetup();
+  simulationSetup();
+  findMeltStep();
+  simulationSetup();
+  findMeltStep2();
+  simulationSetup();
 }
 
 /*
- * Function: measureSimulation
  * The actual simulation. Calculates temperature and plots it.
- * Author: Daniel Prudente (September 2012)
- * Modder: Daniel Vasquez (2017)
  */
 function measureSimulation() {
-	if (currentStep < numDataPts) {
-		Qmelt = (waterT-(Cice+CtoKelvin))*h*surfArea*timeStep;
-		//alert("Qmelt: " + Qmelt);
-		iceMelt = Qmelt/hfusion;
-		//alert("iceMelt: " + iceMelt);
-		iceMassOld = iceMass;
-		iceMass = iceMassOld - iceMelt;
-		//alert("iceMass: " + iceMass);
-		if (iceMass > 0.1) {
-			tempT = waterT - Qmelt/((iceMelt+waterMass)*joulesPerCal);
-			//alert("tempT: " + tempT);
-			waterT = (waterMass*tempT + iceMelt*(Cice+CtoKelvin))/(iceMelt+waterMass);
-			//alert("waterT: " + waterT);
-			waterMass = waterMass + iceMelt;
-			//alert("waterMass: " + waterMass);
-			surfArea = iceMass/iceMassOld*surfArea;
-		}
+  if (currentStep < numDataPts) {
+    Qmelt = (waterT-(Cice+CtoKelvin))*h*surfArea*timeStep;
+    //alert("Qmelt: " + Qmelt);
+    iceMelt = Qmelt/hfusion;
+    //alert("iceMelt: " + iceMelt);
+    iceMassOld = iceMass;
+    iceMass = iceMassOld - iceMelt;
+    //alert("iceMass: " + iceMass);
+    if (iceMass > 0.1) {
+      tempT = waterT - Qmelt/((iceMelt+waterMass)*joulesPerCal);
+      //alert("tempT: " + tempT);
+      waterT = (waterMass*tempT + iceMelt*(Cice+CtoKelvin))/(iceMelt+waterMass);
+      //alert("waterT: " + waterT);
+      waterMass = waterMass + iceMelt;
+      //alert("waterMass: " + waterMass);
+      surfArea = iceMass/iceMassOld*surfArea;
+    }
 
-		Qmelt2 = (waterT2-(Cice+CtoKelvin))*h*surfArea2*timeStep;
-		iceMelt2 = Qmelt2/hfusion;
-		iceMassOld2 = iceMass2;
-		iceMass2 = iceMass2 - iceMelt2;
-		if (iceMass2 > 0.1) {
-			tempT2 = waterT2 - Qmelt2/((iceMelt2+waterMass2)*joulesPerCal);
-			waterT2 = (waterMass2*tempT2 + iceMelt2*(Cice+CtoKelvin))/(iceMelt2+waterMass2);
-			waterMass2 = waterMass2 + iceMelt2;
-			surfArea2 = iceMass2/iceMassOld2*surfArea2;
-		}
+    Qmelt2 = (waterT2-(Cice+CtoKelvin))*h*surfArea2*timeStep;
+    iceMelt2 = Qmelt2/hfusion;
+    iceMassOld2 = iceMass2;
+    iceMass2 = iceMass2 - iceMelt2;
+    if (iceMass2 > 0.1) {
+      tempT2 = waterT2 - Qmelt2/((iceMelt2+waterMass2)*joulesPerCal);
+      waterT2 = (waterMass2*tempT2 + iceMelt2*(Cice+CtoKelvin))/(iceMelt2+waterMass2);
+      waterMass2 = waterMass2 + iceMelt2;
+      surfArea2 = iceMass2/iceMassOld2*surfArea2;
+    }
 
-		// Plots every other data point
-		if (currentStep % 2 == 0) {
-			x1 = 38 + currentStep + "px";
-			x2 = 39 + currentStep + "px";
+    // Plots every other data point
+    if (currentStep % 2 == 0) {
+      x1 = 38 + currentStep + "px";
+      x2 = 39 + currentStep + "px";
 
-			y1 = (graphHeight - (waterT-CtoKelvin)*(graphHeight/maxTemp) + 68); + "px";
-			y2 = (graphHeight - (waterT2-CtoKelvin)*(graphHeight/maxTemp) + 68); + "px";
+      y1 = (graphHeight - (waterT-CtoKelvin)*(graphHeight/maxTemp) + 68); + "px";
+      y2 = (graphHeight - (waterT2-CtoKelvin)*(graphHeight/maxTemp) + 68); + "px";
 
-			if ((y2-y1) < 1.5) {
-				y2 = y2 + "px";
-				y1 = y2;
-			} else {
-				y1 = y1 + "px";
-				y2 = y2 + "px";
-			}
+      if ((y2-y1) < 1.5) {
+        y2 = y2 + "px";
+        y1 = y2;
+      } else {
+        y1 = y1 + "px";
+        y2 = y2 + "px";
+      }
 
-			//alert(y1 + "    " + y2);
+      var dot1 = "#sit1Point" + (currentStep/2);
+      var dot2 = "#sit2Point" + (currentStep/2);
+      $(dot1).css({top:y1, left:x1});
+      $(dot2).css({top:y2, left:x2});
+      $(dot1).show();
+      $(dot2).show();
+    }
 
-			var dot1 = "#sit1Point" + (currentStep/2);
-			var dot2 = "#sit2Point" + (currentStep/2);
-			$(dot1).css({top:y1, left:x1});
-			$(dot2).css({top:y2, left:x2});
-			$(dot1).show();
-			$(dot2).show();
-		}
-
-		currentStep++;
-		setTimeout(measureSimulation, 40);
-	} else {
-		$("#resetButton").removeAttr("disabled");
-	}
+    currentStep++;
+    setTimeout(measureSimulation, 40);
+  } else {
+    $("#resetButton").removeAttr("disabled");
+  }
 }
 
-/* rgb(75, 193, 101) - END Maths and Sciences Functions ------------------*/
+/************** Animation functions *****************/
 
-/* rgb(116, 52, 233) - BEGIN Animation Functions ------------------ */
-
-/* Toggles holding the hammer. Replaces the cursor with a hammer graphic.
- *
- * Author: Daniel Vasquez (2017)
+/* 
+ * Toggles holding the hammer. Replaces the cursor with a hammer graphic.
  */
 function toggleHammer() {
   holdingHammer = !holdingHammer;
-  if (holdingHammer){
+  if (holdingHammer) {
     cursor('hammer_hover.cur', 0, 0);
   } else {
     cursor(ARROW, 0, 0);
   }
 }
 
-/* Attempts to break the ice further.
- * If maxDivisions is reached, does nothing.
- * Author: Daniel Vasquez (2017)
+/* 
+ * Attempts to break the ice further. Does nothing if MAX_DIVISIONS is reached.
  */
-function swingHammer(exp) {
-  if (exp.numDivisions < maxDivisions){
+function swingHammer() {
+  if (brokenExp.numDivisions < MAX_DIVISIONS) {
     print("Breaking ice");
-    exp.numDivisions += 1;
+    brokenExp.numDivisions += 1;
     breakAnimation();
-    setDivisions(exp.numDivisions, exp);
+    brokenExp.setDivisions(brokenExp.numDivisions);
   }
+
   else {
     print("The ice couldn't be broken further");
-    noBreakAnimation(); // Animation that signals to user that ice can not be broken further
+    noBreakAnimation();
   }
 }
 
-/* Animation for the breaking of user-breakable ice block.
- * Author: Daniel Vasquez (2017)
+/* 
+ * Animation for the breaking of user-breakable ice block.
  */
-function breakAnimation(){
+function breakAnimation() {
   // Spawn strike sparks
   return
 }
 
-/* Animation indicating that the ice couldn't be broken.
- * Author: Daniel Vasquez (2017)
+/* 
+ * Animation indicating that the ice couldn't be broken.
  */
-function noBreakAnimation(){
+function noBreakAnimation() {
   // Spawn dust/poof/smoke particles
   return
 }
 
-/* rgb(116, 52, 233) - END Animation Functions ------------------ */
-
-/* rgb(252, 220, 49) - BEGIN User Interaction Functions ------------------ */
+/*********** User interaction functions *************/
 
 function mousePressed(){
-  swingHammer(brknExp);
+  swingHammer();
 }
-
-/* rgb(252, 220, 49) - END User Interaction Functions ------------------ */
