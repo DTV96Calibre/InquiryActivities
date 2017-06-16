d99fd1a35b760a3bb9f1a4640bb13664228facc8
DTV96Calibre <dtv96calibre@gmail.com>
2017-06-15 20:21:16 -0400

    Initialized hot pots prototype in Godot Engine

---
+++ Priority_2_Thermodynamics/Hot_Pots/chef_arm.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+
+func _process(delta):
+	self.rotate(.1)
---
+++ Priority_2_Thermodynamics/Hot_Pots/pot.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+
+func _process(delta):
+	self.set_global_rot(0)
