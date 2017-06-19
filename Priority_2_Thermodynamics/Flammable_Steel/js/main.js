f80efd0eaee58f9c82578a7493a76aff6d5ac315
Brooke Bullek <bbullek@live.com>
2017-06-19 17:44:22 -0400

    Steel wool catches fire on mouseover

--- Priority_2_Thermodynamics/Flammable_Steel/js/main.js
+++ Priority_2_Thermodynamics/Flammable_Steel/js/main.js
@@ -13,6 +13,8 @@ var canvas;
 var ctx;
 var fire;
 
+var mousedOverWool = false;
+
 var img;
 
 function setup() {
@@ -25,14 +27,36 @@ function setup() {
 
   // Load image from URL
   img = createImg("https://github.com/DTV96Calibre/InquiryActivities/blob/master/Priority_2_Thermodynamics/Flammable_Steel/images/steel_0.png?raw=true");
+  img2 = createImg("https://github.com/DTV96Calibre/InquiryActivities/blob/master/Priority_2_Thermodynamics/Flammable_Steel/images/steelwool.png?raw=true");
+  img2fire = createImg("https://github.com/DTV96Calibre/InquiryActivities/blob/master/Priority_2_Thermodynamics/Flammable_Steel/images/steelwool-fire.png?raw=true");
   img.hide();
+  img2.hide();
+  img2fire.hide();
 }
 
 function draw() {
   background("blue"); // Clear the canvas
   background("rgba( 15, 5, 2, 1 )");
 
-  image(img, windowWidth / 2 - img.elt.width / 2, 
-  	windowHeight / 2 - img.elt.height / 2, img.elt.width / 2, img.elt.height / 2);
+  if (cursorOverSteelWool()) {
+  	mousedOverWool = true;
+  }
+
+  image(img, windowWidth / 5, 
+  	windowHeight / 3, img.elt.width / 2, img.elt.height / 2);
+
+  if (mousedOverWool) {
+  	image(img2fire, windowWidth / 2, 
+  		windowHeight / 3, img.elt.width / 2, img.elt.height / 2);
+  } else {
+	image(img2, windowWidth / 2, 
+  		windowHeight / 3, img.elt.width / 2, img.elt.height / 2);
+  }
+  
   fire.update();
+}
+
+function cursorOverSteelWool() {
+	return (mouseX > windowWidth / 2 && mouseX < windowWidth / 2 + img.elt.width / 2
+		&& mouseY > windowHeight / 3 && mouseY < windowHeight / 3 + img.elt.height / 2);
 }
