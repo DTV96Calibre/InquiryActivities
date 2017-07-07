19cf580bf445bf685cfda1a875a2af34782a60ed
DTV96Calibre <dtv96calibre@gmail.com>
2017-07-07 13:07:12 -0400

    Wording

--- Priority_2_Thermodynamics/Heat_Conduction/js/Intro.js
+++ Priority_2_Thermodynamics/Heat_Conduction/js/Intro.js
@@ -3,13 +3,13 @@ function Intro()
 {
     this.s;
     this.setup = function() {
-      s = "The quick brown fox jumped over the lazy dog.";
+      s = "The cook on the SS Salty Kitty has lived a dangerous life cooking for the most eccentric pirates on these seven seas. After his most recent accident involving a poorly designed pot, he's hired you to design a handle that won't burn him to death. He's entrusted his last 3 lives to you...";
     }
 
     this.draw = function() {
       clear();
       fill(50);
-      text(s, 10, 10, 70, 80); // Text wraps within text box
+      text(s, 10, 10, 400, 600); // Text wraps within text box
     }
     this.mousePressed = function() {
         print("mouse pressed");
