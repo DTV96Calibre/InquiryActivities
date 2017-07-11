7e6c1d0f3f2ad2f655035f33d154dd3937ac9261
DTV96Calibre <dtv96calibre@gmail.com>
2017-07-11 15:10:05 -0400

    joint.pos relative to anchorPoint, locked to left of last. highlighting

--- Priority_2_Thermodynamics/Heat_Conduction/js/Editor.js
+++ Priority_2_Thermodynamics/Heat_Conduction/js/Editor.js
@@ -21,7 +21,7 @@ function Editor(){
       pot = new Pot({x:windowWidth-POT_H_OFFSET, y:windowHeight/2}, 51);
       arm = new Arm([100, 100]);
 
-      joints.push(new Joint(100, null, pot.anchorPoint));
+      joints.push(new Joint(100, null, {x:0, y:0}));
 
       // Tell sceneManager setup is finished before resizing canvas
       this.sceneManager.scene.setupExecuted = true;
@@ -43,30 +43,27 @@ function Editor(){
       arm.setPos([mouseX, mouseY]);
       arm.draw();
       //print(cos(0.7853981633974483 + HALF_PI));
+      highlightNearestJoint();
     }
 
     this.windowResized = function() {
       var HEIGHT_OF_SLIDER = 25;
       //diameterSlider.style('height', '25');
       pot.pos.x = windowWidth-POT_H_OFFSET;
+      pot.locateAnchorPoint();
       resizeCanvas(windowWidth, windowHeight-HEIGHT_OF_SLIDER);
       print("Resized canvas");
     }
 
     this.mouseClicked = function() {
       if (mouseY < 200) {
+        print("placing");
         var radius = diameterSlider.value();
         insertJoint(mouseX, pot.anchorPoint.y, radius);
         print(joints);
       }
     }
 
-}
-
-function demo1() {
-  insertJoint(200, 700, 10);
-  insertJoint(200, 100, 50);
-  insertJoint(600, 300, 40);
 }
 
 /* Given a tap location and radius of new joint, creates and links
@@ -77,9 +74,27 @@ function demo1() {
  * @param radius: radius of new joint
  */
 function insertJoint(x, y, radius) {
-  joints.push(new Joint(radius, joints[joints.length-1], {x:x, y:y}));
+  // Joints are drawn relative to the anchorpoint so calculate offset
+  var jointXOffset = x - pot.anchorPoint.x;
+  var jointYOffset = y - pot.anchorPoint.y;
+
+  // Limit new joints to only be created to the left of the previous joints
+  if (jointXOffset >= joints[joints.length-1].pos.x) {
+    return; //TODO: Here we should try removing nodes until valid
+  }
+
+  joints.push(new Joint(radius, joints[joints.length-1], {x:jointXOffset, y:jointYOffset}));
   joints[joints.length-2].next = joints[joints.length-1];
 }
+
+function getDistance(pos1, pos2){
+  var x1 = pos1.x;
+  var y1 = pos1.y;
+  var x2 = pos2.x;
+  var y2 = pos2.y;
+  return abs(sqrt(sq(x2-x1) + sq(y2-y1)));
+}
+
 /* Recursively calculates temperature of each node in the pipe chain.
  * @param currentJoint: A joint whose temperature is already known.
  */
@@ -94,4 +109,34 @@ function heatTransferTraverse(currentJoint){
 
 function getNewTemp(d1, d2, length, t1){
   return t1 - t1/10;
+}
+
+/* Returns the joint that is closest to a given position pos.
+ */
+function getNearestJoint(pos) {
+  var relativePos = {x:pos.x - pot.anchorPoint.x, y:pos.y - pot.anchorPoint.y};
+  var nearest = 0; //index of nearest
+  var current = 0;
+  var length = joints.length; // length of the list of joints
+  var distance = getDistance(joints[nearest].pos, relativePos);
+  var nextDistance;
+  for (current = 1; current < length; current += 1){
+    print("loop");
+    nextDistance = getDistance(joints[current].pos, relativePos);
+    if (distance > nextDistance) {
+      nearest = current;
+      distance = nextDistance;
+    }
+  }
+  print("mousePos: " + pos);
+  print("relativePos: " + relativePos);
+  print("nearestDistance: " + distance);
+  return joints[nearest];
+}
+
+function highlightNearestJoint(){
+  var nearest = getNearestJoint({x:mouseX, y:mouseY});
+  stroke(255);
+  ellipse(nearest.pos.x + pot.anchorPoint.x, nearest.pos.y + pot.anchorPoint.y, nearest.radius);
+  noStroke();
 }
--- Priority_2_Thermodynamics/Heat_Conduction/js/Joint.js
+++ Priority_2_Thermodynamics/Heat_Conduction/js/Joint.js
@@ -2,11 +2,11 @@ class Joint{
   constructor(radius, prev, pos){
     this.radius = radius;
     this.prev = prev;
-    this.pos = pos; //TODO: Make this relative to anchor point
+    this.pos = pos; //NOTE: This is relative to the the global pot.anchorPoint object of form {x:_, y:_}
     this.next = null;
   }
   draw(){
-    ellipse(this.pos.x, this.pos.y, this.radius);
+    ellipse(this.pos.x + pot.anchorPoint.x, this.pos.y + pot.anchorPoint.y, this.radius);
     if (this.next) {
       Joint.drawPipe(this.pos, this.radius, this.next.pos, this.next.radius);
       this.next.draw();
@@ -37,14 +37,18 @@ class Joint{
     var p2 = Joint.pointFromCircle(rad2, normalMinus90);
     var p4 = Joint.pointFromCircle(rad1, normalPlus90);
     var p3 = Joint.pointFromCircle(rad2, normalPlus90);
-    p1.x += pos1.x;
-    p1.y += pos1.y;
-    p2.x += pos2.x;
-    p2.y += pos2.y;
-    p4.x += pos1.x;
-    p4.y += pos1.y;
-    p3.x += pos2.x;
-    p3.y += pos2.y;
+    var pos1xAbsolute = pos1.x + pot.anchorPoint.x;
+    var pos1yAbsolute = pos1.y + pot.anchorPoint.y;
+    var pos2xAbsolute = pos2.x + pot.anchorPoint.x;
+    var pos2yAbsolute = pos2.y + pot.anchorPoint.y;
+    p1.x += pos1xAbsolute;
+    p1.y += pos1yAbsolute;
+    p2.x += pos2xAbsolute;
+    p2.y += pos2yAbsolute;
+    p4.x += pos1xAbsolute;
+    p4.y += pos1yAbsolute;
+    p3.x += pos2xAbsolute;
+    p3.y += pos2yAbsolute;
     quad(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
   }
 }
