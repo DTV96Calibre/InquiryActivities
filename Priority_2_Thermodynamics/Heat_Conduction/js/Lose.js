af00bdf30b37f821943b6f2b64ee4f3e0d9d4aa9
Brooke Bullek <bbullek@live.com>
2017-08-13 13:05:46 -0400

    Matched cat image aspect ratio between scenes

--- Priority_2_Thermodynamics/Heat_Conduction/js/Lose.js
+++ Priority_2_Thermodynamics/Heat_Conduction/js/Lose.js
@@ -41,7 +41,7 @@ function Lose()
     }
 
     this.drawCat = function() {
-      image(images['cat'], windowWidth * 0.4, windowHeight * 0.25, windowWidth * 0.2, windowWidth * 0.17);
+      image(images['cat'], windowWidth * 0.42, windowHeight * 0.25, windowWidth * 0.16, windowWidth * 0.17);
       /*
       var scaleFactor = 2;
       var headCenter = [width/(2*scaleFactor),height/(2*scaleFactor)];
